e7c2529f80da244420d2984a253f125b378745f8
Troy Will <troydwill@gmail.com>
2010-09-22 19:06:49 -0700

    minor

---
+++ bin/030_collect_data.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+open ( my $datafile, '>>', '030_datafile.txt' ) or die "Unable to open 030_datafile for appending";
+
+&uname;
+&lspci;
+
+sub uname {
+    my $kernel_release = `uname -r`;
+    print $datafile "Output from uname -r ................................\n";
+    print $datafile $kernel_release, "\n";
+}
+
+sub lspci {
+    # chomp( my $lspci_bin = `which lspci` );
+    # print "------- $lspci_bin ----\n";
+    my $lspci = `lspci`;
+    print $datafile "\nOutput from lspci ................................\n";
+    print $datafile $lspci;
+}
---
+++ bin/040_install_alsa_1.0.23_backport.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# http://ubuntuforums.org/showthread.php?t=1552250, message 8, 2010-09-22
+sudo apt-get install linux-backports-modules-alsa-2.6.32-24-generic alsa-utils
